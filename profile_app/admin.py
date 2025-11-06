11e2c98603dfad00e22cf41be11dccce88f617a5
TobiasKlanert <tobias.klanert@t-online.de>
2025-11-06 21:37:11 +0100

    feat: profile_app - User profiles registered in the admin panel

--- profile_app/admin.py
+++ profile_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'type']
+    list_filter = ['type']
+
+    def first_name(self, obj):
+        return obj.user.first_name
+    first_name.admin_order_field = 'user__first_name'  # type: ignore[attr-defined]
+    first_name.short_description = 'First name'  # type: ignore[attr-defined]
+
+    def last_name(self, obj):
+        return obj.user.last_name
+    last_name.admin_order_field = 'user__last_name'  # type: ignore[attr-defined]
+    last_name.short_description = 'Last name'  # type: ignore[attr-defined]
+
+
+admin.site.register(UserProfile, ProfileAdmin)
