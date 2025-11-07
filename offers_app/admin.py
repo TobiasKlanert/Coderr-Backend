7d718cc7221ed337edf5543acca46bce9583430b
TobiasKlanert <tobias.klanert@t-online.de>
2025-11-07 17:40:08 +0100

    feat: offers_app - Offers registered in the admin panel

--- offers_app/admin.py
+++ offers_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Offer, Detail
 
-# Register your models here.
+
+class DetailInline(admin.TabularInline):
+    model = Detail
+    extra = 0
+    fields = ['offer_type', 'title', 'price', 'delivery_time_in_days', 'revisions', 'features']
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'created_at', 'updated_at', 'min_price']
+    list_filter = ['user']
+    inlines = [DetailInline]
+
+
+admin.site.register(Offer, OfferAdmin)
