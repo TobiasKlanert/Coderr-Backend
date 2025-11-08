34fa25e78c15f0172e6c33963c99f3d60487feee
TobiasKlanert <tobias.klanert@t-online.de>
2025-11-08 13:42:11 +0100

    feat: django filters installed

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
     'auth_app',
@@ -135,6 +136,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
     ],
 }
 
