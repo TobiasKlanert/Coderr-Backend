1b1827e30acfaadb1ab29c21089abcbae99d358a
TobiasKlanert <tobias.klanert@t-online.de>
2025-11-12 18:03:06 +0100

    feat: reviews_app - Reviews registered in admin panel

--- reviews_app/admin.py
+++ reviews_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['business_user__username', 'reviewer__username', 'rating', 'created_at']
+    list_filter = ['rating']
+
+admin.site.register(Review, ReviewAdmin)
